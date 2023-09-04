224c6c91a006dc944f94d5a02f196cff6c68d675
Druxorey <137116223+druxorey@users.noreply.github.com>
2023-09-03 23:59:46 -0400

    'startup-programs.sh' update

--- config-files/bash-installers/startup-programs.sh
+++ config-files/bash-installers/startup-programs.sh
@@ -7,20 +7,29 @@ echo
 # Desktop Enviroment
 echo
 echo -e "\e[33m 1) Desktop Enviroment"
-echo -e "\e[95m You will be installing the next packages: \e[0mxorg, bspwm, sxhkd, lxappearance, picom, nitrogen, kitty, lightdm, lightdm-gtk-greeter, lightdm-gtk-greeter-settings and intel-ucode."
+echo -e "\e[95m You will be installing the next packages: \e[0mxorg, bspwm, sxhkd, lxappearance, picom, nitrogen, kitty, lightdm, lightdm-gtk-greeter, lightdm-gtk-greeter-settings and intel-ucode, reflector."
 echo
-sudo pacman -S xorg bspwm sxhkd lxappearance picom nitrogen kitty lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings intel-ucode
+sudo pacman -S xorg bspwm sxhkd lxappearance picom nitrogen kitty lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings intel-ucode reflector
+
+# Reflector Configuration
+
+echo
+echo -e "\e[33m 2) Reflector Configuration"
+echo
+
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 # Terminal Tools
 echo
-echo -e "\e[33m 2) Terminal Tools"
+echo -e "\e[33m 3) Terminal Tools"
 echo -e "\e[95m You will be installing the next packages: \e[0mneovim, gedit, unzip, p7zip, unrar, tar, rsync, htop, exfat-utils, fuse-exfat, curl, wget, trash-cli, ranger, thefuck, tldr, gnome-keyring, usbutils, gthumb, bash-completion, neofetch, vim, git."
 echo
 sudo pacman -S neovim gedit unzip p7zip unrar tar rsync htop exfat-utils fuse-exfat curl wget trash-cli ranger thefuck tldr gnome-keyring usbutils gthumb bash-completion neofetch vim git
 
 # Basic Programs
 echo
-echo -e "\e[33m 3) Desktop Enviroment"
+echo -e "\e[33m 4) Basic Programs"
 echo -e "\e[95m You will be installing the next packages: \e[0mthunar, nautilus, dmenu, vlc, polybar, blueman, bluez-utils, jdk-openjdk, flatpak."
 echo
 sudo pacman -S thunar nautilus dmenu vlc polybar blueman bluez-utils jdk-openjdk flatpak 
